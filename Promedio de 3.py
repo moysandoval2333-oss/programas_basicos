00da4ca6607c8463313ae2275a156e09f9364f6d
Moises Sandoval Acosta <moy.sandoval2333@gmail.com>
2026-07-28 00:50:29 -0600

    Add script to calculate the average of three numbers

---
+++ Promedio de 3.py
@@ -0,0 +1,7 @@
+n1 = float(input("Ingresa el primer número: "))
+n2 = float(input("Ingresa el segundo número: "))
+n3 = float(input("Ingresa el tercer número: "))
+
+promedio = (n1 + n2 + n3) / 3
+
+print("El promedio de estos tres números es:", promedio)
