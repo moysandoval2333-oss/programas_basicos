e60852edc0b742f921c688f458ec0ad0f14b3627
Moises Sandoval Acosta <moy.sandoval2333@gmail.com>
2026-07-27 21:51:22 -0600

    Add area calculation program for rectangle

---
+++ area_rectangulo.py
@@ -0,0 +1,5 @@
+# 1. Programa para calcular el área de un rectángulo 
+base = float(input("Ingresa la base del rectángulo: "))
+altura = float(input("ingresa la altura del rectángulo: "))
+area = base * altura 
+print(f"El área del rectángulo es: {area}")
